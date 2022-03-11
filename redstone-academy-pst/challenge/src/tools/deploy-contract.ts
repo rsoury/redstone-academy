18b28054c879c32b14b413306a0f16be46213101
Ryan Soury <ryan@webdoodle.com.au>
2022-03-11 12:23:17 +1100

    deploying... althought quite slow for block mining

--- redstone-academy-pst/challenge/src/tools/deploy-contract.ts
+++ redstone-academy-pst/challenge/src/tools/deploy-contract.ts
@@ -1,15 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import { SmartWeave, LoggerFactory, SmartWeaveNodeFactory } from 'redstone-smartweave'
+import Arweave from 'arweave';
+import { PstState } from '../contracts/types/types';
+import wallet from '../../.secrets/arconnect-arweave-keyfile.json';
+
 (async () => {
 // ~~ Declare variables ~~
+let contractSrc: string;
+
+let initialState: PstState;
+
+let arweave: Arweave;
+let smartweave: SmartWeave;
 
 // ~~ Initialize Arweave ~~
+arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https',
+});
 
 // ~~ Initialize `LoggerFactory` ~~
+LoggerFactory.INST.logLevel('error');
 
 // ~~ Initialize SmartWeave ~~
+smartweave = SmartWeaveNodeFactory.memCached(arweave)
 
 // ~~ Read contract source and initial state files ~~
+contractSrc = fs.readFileSync(
+  path.join(__dirname, '../../dist/contract.js'),
+  'utf8'
+);
+initialState = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, '../../dist/contracts/initial-state.json'),
+    'utf8'
+  )
+);
 
 // ~~ Deploy contract ~~
+const contractTxId = await smartweave.createContract.deploy({
+  wallet,
+  initState: JSON.stringify(initialState),
+  src: contractSrc
+})
 
 // ~~ Log contract id to the console ~~
+console.log(contractTxId);
 })();
+
+// 12:16pm 11/03/2022 -- oHf7eem8xWN5wWh71c_m8PQatV9DdwBRgFNUSOH8Cd0
--- redstone-academy-pst/challenge/src/tools/deploy-test-contract.ts
+++ redstone-academy-pst/challenge/src/tools/deploy-test-contract.ts
@@ -1,19 +1,69 @@
+import fs from 'fs';
+import path from 'path';
+import { PstContract, SmartWeave, LoggerFactory, SmartWeaveNodeFactory } from 'redstone-smartweave'
+import Arweave from 'arweave';
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import { PstState } from '../contracts/types/types';
+import { addFunds } from '../../utils/_helpers';
+
 (async () => {
-// ~~ Declare variables ~~
+  // ~~ Declare variables ~~
+  let contractSrc: string;
 
-// ~~ Initialize Arweave ~~
+  let wallet: JWKInterface;
+  let walletAddress: string;
 
-// ~~ Initialize `LoggerFactory` ~~
+  let initialState: PstState;
 
-// ~~ Initialize SmartWeave ~~
+  let arweave: Arweave;
+  let smartweave: SmartWeave;
+  let pst: PstContract;
 
-// ~~ Generate wallet and add some funds ~~
+  // ~~ Initialize Arweave ~~
+  arweave = Arweave.init({
+    host: 'testnet.redstone.tools',
+    port: 443,
+    protocol: 'https',
+  });
 
-// ~~ Read contract source and initial state files ~~
+  // ~~ Initialize `LoggerFactory` ~~
+  LoggerFactory.INST.logLevel('error');
 
-// ~~ Override contract's owner address with the generated wallet address ~~
+  // ~~ Initialize SmartWeave ~~
+  smartweave = SmartWeaveNodeFactory.memCached(arweave)
 
-// ~~ Deploy contract ~~
+  // ~~ Generate wallet and add some funds ~~
+  wallet = await arweave.wallets.generate()
+  walletAddress = await arweave.wallets.jwkToAddress(wallet)
+  await addFunds(arweave, wallet);
 
-// ~~ Log contract id to the console ~~
+  // ~~ Read contract source and initial state files ~~
+  contractSrc = fs.readFileSync(
+    path.join(__dirname, '../../dist/contract.js'),
+    'utf8'
+  );
+  const stateFromFile: PstState = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, '../../dist/contracts/initial-state.json'),
+      'utf8'
+    )
+  );
+
+  // ~~ Override contract's owner address with the generated wallet address ~~
+  initialState = {
+    ...stateFromFile,
+    ...{
+      owner: walletAddress,
+    },
+  };
+
+  // ~~ Deploy contract ~~
+  const contractTxId = await smartweave.createContract.deploy({
+    wallet,
+    initState: JSON.stringify(initialState),
+    src: contractSrc
+  })
+
+  // ~~ Log contract id to the console ~~
+  console.log(contractTxId);
 })();
